319aae6a1d6cf3d67f6a5597c0ba6d483e96b437
Jhinwins <zouheng613@163.com>
2017-07-27 17:45:53 +0800

    1.更新项目为springmvc项目 2.修改jdk为1.8

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## [网易云音乐的爬虫](http://59.110.143.71/CMSpider4web/)
 
-[![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()    
-可以完成网易云音乐评论的搜索    
-界面基于bootstrap构建，基本可实现自适应    
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()<br/>
+可以完成网易云音乐评论的搜索<br/>
+界面基于bootstrap构建，基本可实现自适应<br/>
 
-![image](https://github.com/Jhinwins/cloudMusicSpider/blob/master/imgs/index.png)    
-![image](https://github.com/Jhinwins/cloudMusicSpider/blob/master/imgs/comments_index.png)    
-![image](https://github.com/Jhinwins/cloudMusicSpider/blob/master/imgs/search_comments.png)    
+![image](https://github.com/Jhinwins/cloudMusicSpider/blob/master/imgs/index.png)<br/>
+![image](https://github.com/Jhinwins/cloudMusicSpider/blob/master/imgs/comments_index.png)<br/>
+![image](https://github.com/Jhinwins/cloudMusicSpider/blob/master/imgs/search_comments.png)<br/>
 ![image](https://github.com/Jhinwins/cloudMusicSpider/blob/master/imgs/comments.png)
--- src/main/java/com/jhinwins/utils/IpUtils.java
+++ src/main/java/com/jhinwins/utils/IpUtils.java
@@ -23,132 +23,131 @@ import jhinwins.core.impl.SimpleProxyIpSpider;
 import jhinwins.model.ProxyIp;
 
 public class IpUtils {
-	private final static int IPPOOL_MIN_COUNT = 4;
-	private static List<ProxyIp> proxyips = new ArrayList<>();
+    private final static int IPPOOL_MIN_COUNT = 4;
+    private static List<ProxyIp> proxyips = new ArrayList<ProxyIp>();
 
-	private static boolean loadingIps = false;
+    private static boolean loadingIps = false;
 
-	public static SimpleProxyIpSpider simpleProxyIpSpider = new SimpleProxyIpSpider("http://www.kuaidaili.com/free/inha/2/") {
-		@Override
-		public Elements parseIPHome(Document html) {
-			Elements elements = html.select("tr");
-			return elements;
-		}
+    public static SimpleProxyIpSpider simpleProxyIpSpider = new SimpleProxyIpSpider("http://www.kuaidaili.com/free/inha/2/") {
+        @Override
+        public Elements parseIPHome(Document html) {
+            Elements elements = html.select("tr");
+            return elements;
+        }
 
-		@Override
-		public String parseIP(Element element) {
-			return element.child(0).text();
-		}
+        @Override
+        public String parseIP(Element element) {
+            return element.child(0).text();
+        }
 
-		@Override
-		public Integer parsePort(Element element) {
-			return Integer.parseInt(element.child(1).text());
-		}
+        @Override
+        public Integer parsePort(Element element) {
+            return Integer.parseInt(element.child(1).text());
+        }
 
-	};
+    };
 
-	/**
-	 * 获取一个代理ip
-	 * 
-	 * @return 代理ip对象 如果没有则返回空
-	 */
-	public static ProxyIp pullProxyIp() {
-		ProxyIp proxyip = null;
-		if (proxyips.size() > 0) {
-			proxyip = proxyips.get(0);
-			proxyips.remove(0);
-		}
-		// 如果ip池中的ip数量较少则再添加
-		if (proxyips.size() < IPPOOL_MIN_COUNT && !loadingIps) {
-			loadingIps = true;
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					addProxyIp();
-					loadingIps = false;
-				}
-			}).start();
-		}
+    /**
+     * 获取一个代理ip
+     *
+     * @return 代理ip对象 如果没有则返回空
+     */
+    public static ProxyIp pullProxyIp() {
+        ProxyIp proxyip = null;
+        if (proxyips.size() > 0) {
+            proxyip = proxyips.get(0);
+            proxyips.remove(0);
+        }
+        // 如果ip池中的ip数量较少则再添加
+        if (proxyips.size() < IPPOOL_MIN_COUNT && !loadingIps) {
+            loadingIps = true;
+            new Thread(new Runnable() {
+                public void run() {
+                    addProxyIp();
+                    loadingIps = false;
+                }
+            }).start();
+        }
 
-		if (proxyip == null) {
-			try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return pullProxyIp();
-		}
+        if (proxyip == null) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return pullProxyIp();
+        }
 
-		return proxyip;
-	}
+        return proxyip;
+    }
 
-	private static void addProxyIp() {
-		CloseableHttpClient httpClient = null;
-		BufferedReader reader = null;
-		try {
-			httpClient = HttpClients.createDefault();
-			HttpGet httpGet = new HttpGet("http://www.xdaili.cn/ipagent//freeip/getFreeIps");
-			CloseableHttpResponse response = httpClient.execute(httpGet);
-			reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "utf-8"));
-			String res = "";
-			String buff;
-			while ((buff = reader.readLine()) != null) {
-				res += buff;
-			}
+    private static void addProxyIp() {
+        CloseableHttpClient httpClient = null;
+        BufferedReader reader = null;
+        try {
+            httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet("http://www.xdaili.cn/ipagent//freeip/getFreeIps");
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+            reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "utf-8"));
+            String res = "";
+            String buff;
+            while ((buff = reader.readLine()) != null) {
+                res += buff;
+            }
 
-			JSONObject res_json = JSON.parseObject(res);
-			JSONArray ips = res_json.getJSONArray("rows");
-			Iterator<Object> iterator = ips.iterator();
+            JSONObject res_json = JSON.parseObject(res);
+            JSONArray ips = res_json.getJSONArray("rows");
+            Iterator<Object> iterator = ips.iterator();
 
-			while (iterator.hasNext()) {
-				JSONObject ipItem = (JSONObject) iterator.next();
-				String ip = ipItem.getString("ip");
-				String port_str = ipItem.getString("port");
-				Integer port = Integer.parseInt(port_str);
+            while (iterator.hasNext()) {
+                JSONObject ipItem = (JSONObject) iterator.next();
+                String ip = ipItem.getString("ip");
+                String port_str = ipItem.getString("port");
+                Integer port = Integer.parseInt(port_str);
 
-				ProxyIp proxyIp = new ProxyIp();
-				proxyIp.setIp(ip);
-				proxyIp.setPort(port);
-				if (!isContainIp(proxyIp))
-					proxyips.add(proxyIp);
-			}
+                ProxyIp proxyIp = new ProxyIp();
+                proxyIp.setIp(ip);
+                proxyIp.setPort(port);
+                if (!isContainIp(proxyIp))
+                    proxyips.add(proxyIp);
+            }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-					// do nothing.
-				}
-			}
-			if (httpClient != null) {
-				try {
-					httpClient.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-					// do nothing.
-				}
-			}
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    // do nothing.
+                }
+            }
+            if (httpClient != null) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    // do nothing.
+                }
+            }
+        }
+    }
 
-	/**
-	 * 是否已经包含了这个ip
-	 * 
-	 * @param ip
-	 * @return
-	 */
-	private static boolean isContainIp(ProxyIp ip) {
-		Iterator<ProxyIp> iterator = proxyips.iterator();
-		while (iterator.hasNext()) {
-			if (iterator.next().getIp().equals(ip.getIp())) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * 是否已经包含了这个ip
+     *
+     * @param ip
+     * @return
+     */
+    private static boolean isContainIp(ProxyIp ip) {
+        Iterator<ProxyIp> iterator = proxyips.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getIp().equals(ip.getIp())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
