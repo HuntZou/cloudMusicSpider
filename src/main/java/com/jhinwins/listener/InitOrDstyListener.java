b8241488b819da8ac072c5cefb7dd3511792fe6c
Jhinwins <zouheng613@163.com>
2017-08-22 16:37:19 +0800

    修复了无法加载代理ip的问题

--- src/main/java/com/jhinwins/listener/InitOrDstyListener.java
+++ src/main/java/com/jhinwins/listener/InitOrDstyListener.java
@@ -3,9 +3,12 @@ package com.jhinwins.listener;
 import jhinwins.NetFilter.Impl.CMNetFilter;
 import jhinwins.core.Action;
 import jhinwins.core.impl.SimpleProxyIpSpider;
+import org.apache.log4j.Logger;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -15,8 +18,11 @@ import javax.servlet.ServletContextListener;
  * Desc:
  */
 public class InitOrDstyListener implements ServletContextListener {
+    private Logger logger = Logger.getLogger(InitOrDstyListener.class);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+
 
         final SimpleProxyIpSpider simpleProxyIpSpider = new SimpleProxyIpSpider("http://www.xicidaili.com/nn/") {
             @Override
@@ -41,13 +47,36 @@ public class InitOrDstyListener implements ServletContextListener {
 //            }
         };
 
+        final SimpleProxyIpSpider simpleProxyIpSpider2 = new SimpleProxyIpSpider("http://www.kuaidaili.com/free/inha/") {
+            @Override
+            public Elements parseIPHome(Document html) {
+                Elements elements = html.select("tr");
+                return elements;
+            }
+
+            @Override
+            public String parseIP(Element element) {
+                return element.child(0).text();
+            }
+
+            @Override
+            public Integer parsePort(Element element) {
+                return Integer.parseInt(element.child(1).text());
+            }
+
+//            @Override
+//            public String parseAnonLevel(Element element) {
+//                return element.select("td[data-title='匿名度']").first().text();
+//            }
+        };
+
         final Action action = new Action();
         new Thread(new Runnable() {
             public void run() {
                 while (true) {
                     action.loadOriginalSource(simpleProxyIpSpider);
                     try {
-                        Thread.sleep(5 * 60 * 1000);
+                        Thread.sleep((int) ((5 + Math.random()) * 60 * 1000));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -58,12 +87,12 @@ public class InitOrDstyListener implements ServletContextListener {
         new Thread(new Runnable() {
             public void run() {
                 while (true) {
-                    action.detectionFirst(new CMNetFilter(), "CMProxyIpPool");
                     try {
                         Thread.sleep(30 * 1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    action.detectionFirst(new CMNetFilter(), "CMProxyIpPool");
                 }
             }
         }).start();
